72ab163ed6ea2c43708b81335996713763713edb
ashish <ashisharora.dev@gmail.com>
2022-03-27 19:37:41 +0530

    updated index.js

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -5,10 +5,22 @@ const SECRET = "CIGAR"; // You can set any word as the secret answer
 function myFunction(req, res) {
 	// console.log({req}); // You can uncomment this to see the request object
 	console.log(req.url);
-
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
-
+	let feedback = "";
+	if (req.url.includes('wordle?q=')){
+		const GUESS = req.url.split('?q=')[1]; // Write logic to parse the word which the user guessed from the URL string
+		for(let i = 0 ; i < 5; i ++){
+			if (GUESS[i] == SECRET[i]){
+				feedback += 'g';
+			}else if(SECRET.includes(GUESS[i])){
+				feedback += 'y';
+			}else{
+				feedback += 'b';
+			}
+		}
+	}else{
+		feedback = "ERROR, please enter url in correct format /wordle?q=<word>"
+	}
+	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write(feedback);
 	res.end();
 }
